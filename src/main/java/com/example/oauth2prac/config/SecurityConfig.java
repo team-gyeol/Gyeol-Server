9085a7dba543a56a059894ff89070c728da9f9a3
dydwls2227 <dydwls2227@naver.com>
2025-10-31 22:08:38 +0900

    swagger security 추가

--- src/main/java/com/example/oauth2prac/config/SecurityConfig.java
+++ src/main/java/com/example/oauth2prac/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/css/**", "/images/**", "/js/**","/swagger-ui/**").permitAll()
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**","/swagger-ui/**","/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .logout(logout -> logout.logoutSuccessUrl("/"))
