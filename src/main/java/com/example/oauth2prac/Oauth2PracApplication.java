dacfbc95338bdfc18f21b4b121fa2917241b34b7
dydwls2227 <dydwls2227@naver.com>
2025-11-06 13:16:37 +0900

    시간 추가 + 이미지 조회 페이징 처리

--- src/main/java/com/example/oauth2prac/Oauth2PracApplication.java
+++ src/main/java/com/example/oauth2prac/Oauth2PracApplication.java
@@ -2,8 +2,10 @@ package com.example.oauth2prac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Oauth2PracApplication {
 
     public static void main(String[] args) {
