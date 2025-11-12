c228c13dd39a3fda323e260dc69ba22752f59c39
dydwls2227 <dydwls2227@naver.com>
2025-11-12 13:21:21 +0900

    CORS 설정

---
+++ src/main/java/com/example/oauth2prac/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.oauth2prac.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
