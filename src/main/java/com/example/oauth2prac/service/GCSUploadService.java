8f310001b3f228af583f2bcc52794c600d2194b2
dydwls2227 <dydwls2227@naver.com>
2025-11-12 13:21:40 +0900

    GCS 설정 변경

--- src/main/java/com/example/oauth2prac/service/GCSUploadService.java
+++ src/main/java/com/example/oauth2prac/service/GCSUploadService.java
@@ -1,6 +1,7 @@
 package com.example.oauth2prac.service;
 
 import com.google.cloud.storage.*;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@Getter
 public class GCSUploadService {
 
     private final Storage storage;
