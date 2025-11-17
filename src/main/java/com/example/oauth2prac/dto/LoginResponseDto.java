ec685080688b757813f540f6f5ec2e84c7ec839f
dydwls2227 <dydwls2227@naver.com>
2025-11-17 21:34:49 +0900

    로그인 수동 api 변경

---
+++ src/main/java/com/example/oauth2prac/dto/LoginResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.oauth2prac.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+}
