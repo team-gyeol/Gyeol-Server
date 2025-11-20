32519c7dddfc98240360b1bb02d869a5950eb020
dydwls2227 <dydwls2227@naver.com>
2025-11-20 13:24:27 +0900

    url 리다이렉트 방식

--- src/main/java/com/example/oauth2prac/global/oauth/OAuthController.java
+++ src/main/java/com/example/oauth2prac/global/oauth/OAuthController.java
@@ -3,12 +3,17 @@ package com.example.oauth2prac.global.oauth;
 import com.example.oauth2prac.global.jwt.LoginResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +28,26 @@ public class OAuthController {
         return oAuthService.loginWithKakao(code)
                 .map(ResponseEntity::ok);
     }
+//
+//    @Operation(summary = "카카오 로그인 콜백", description = "카카오로부터 인가 코드를 받아 로그인을 처리합니다.")
+//    @GetMapping("/kakao/callback")
+//    public Mono<ResponseEntity<Void>> kakaoCallback(@RequestParam("code") String code) {
+//        return oAuthService.loginWithKakao(code)
+//                .map(tokens -> {
+//                    String targetUrl = UriComponentsBuilder.fromUriString(frontendUrl)
+//                            .queryParam("accessToken", tokens.getAccessToken())
+//                            .queryParam("refreshToken", tokens.getRefreshToken())
+//                            .build()
+//                            .encode(StandardCharsets.UTF_8)
+//                            .toUriString();
+//
+//                    HttpHeaders headers = new HttpHeaders();
+//                    headers.add(HttpHeaders.LOCATION, targetUrl);
+//
+//                    return ResponseEntity
+//                            .status(HttpStatus.FOUND)
+//                            .headers(headers)
+//                            .build();
+//                });
+//    }
 }
